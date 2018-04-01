e14173de95b62c19f7c9533d2dda32e48e1fe35d
Jemaine Brown <jembrown@jemaines-mbp.attlocal.net>
2018-04-01 06:55:00 -0500

    Changed readme

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-AWS S3
+## Local upload
+
+This is just a simple uploader that stores files locally.
+
+### Uses
+
+multer
+superagent
+react-dropzone
+
+### Help from
+
+Traversy Media
--- server/index.js
+++ server/index.js
@@ -1,18 +1,11 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
-// const massive = require('massive');
 const multer = require('multer');
-// const AWS = require('aws-sdk');
-
 
 const app = express();
 
-
 app.use(bodyParser.json());
-// massive(process.env.CONNECTION_STRING)
-//     .then(db => app.set('db', db))
-//     .catch(err => console.log(err));
 
 // Storage
 const storage = multer.diskStorage({
